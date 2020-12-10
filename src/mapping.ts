c1435dbd9c44bda6d8fdaf86346c26c8a33ac348
Ivanone27 <75752230+Ivanone27@users.noreply.github.com>
2020-12-10 20:29:46 +0500

    Add files via upload

---
+++ src/mapping.ts
@@ -0,0 +1,47 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { IDEX, Swap, Transfer, Approval } from "../generated/IDEX/IDEX"
+import { Swap_, Transfer_, Approval_ } from "../generated/schema"
+
+export function handleSwap(event: Swap): void {
+  let entity = Swap_.load(event.params.owner.toHex())
+
+  if (entity == null) {
+    entity = new Swap_(event.params.owner.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.owner = event.params.owner
+  entity.value = event.params.value
+  entity.save()
+}
+
+export function handleTransfer(event: Transfer): void {
+  let entity = Transfer_.load(event.params.from.toHex())
+
+  if (entity == null) {
+    entity = new Transfer_(event.params.from.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+  entity.save()
+}
+
+export function handleApproval(event: Approval): void {
+  let entity = Approval_.load(event.params.owner.toHex())
+
+  if (entity == null) {
+    entity = new Approval_(event.params.owner.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+  entity.save()
+}
